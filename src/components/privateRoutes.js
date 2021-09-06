a822d4062b30f3b31769d568c4d9139f63b15111
Hamdy Shabeeb <hamdysh2010@hotmail.com>
2021-09-06 09:25:23 +0200

    feat:add 404 error

--- src/components/privateRoutes.js
+++ src/components/privateRoutes.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import NotFound404 from './NotFound404';
 
 function PrivateRoutes() {
 	return (
 		<Switch>
 			<Route path="/" exact component={Home} />
+			<Route component={NotFound404} />
 		</Switch>
 	);
 }

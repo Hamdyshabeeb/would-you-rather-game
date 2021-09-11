bbc966fea63ac00e7a878f92a098ba0f2aae4953
Hamdy Shabeeb <hamdysh2010@hotmail.com>
2021-09-11 23:40:00 +0200

    feat:add message in question list if it is empty

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -7,10 +7,14 @@ class QuestionList extends Component {
 		const { questions } = this.props;
 		return (
 			<ul className="ps-0 mx-auto" style={{ maxWidth: '540px' }}>
-				{questions.length > 0 &&
+				{(questions.length > 0 &&
 					questions.map((question) => (
 						<Question key={question} id={question} />
-					))}
+					))) || (
+					<li className="mt-5 list-unstyled">
+						<p>This List Is Empty</p>
+					</li>
+				)}
 			</ul>
 		);
 	}
